fc14f8abefc742c986ac4c22808e78cb1553c3ce
lives4code <jacobmurleyis@gmail.com>
2021-09-13 16:50:56 -0400

    made Client and Server not public in samples

--- Files_From_Prof/SampleServer.java
+++ Files_From_Prof/SampleServer.java
@@ -4,7 +4,7 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.util.*;
 
-public class Server {
+class Server {
 
 	private static final int sPort = 8000;   //The server will be listening on this port number
 
