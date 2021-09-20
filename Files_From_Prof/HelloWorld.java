a394a76015ea9bec53dd9525b0d2eb6528ff4862
lives4code <jacobmurleyis@gmail.com>
2021-09-20 16:32:20 -0400

    finished byte translation for handshake

--- Files_From_Prof/HelloWorld.java
+++ Files_From_Prof/HelloWorld.java
@@ -2,10 +2,23 @@ import java.io.*;
 public class HelloWorld {
     public static void main(String[] args)
     {
+        byte[] bytes = new byte[32];
         String hexString = "P2PFILESHARINGPROJ";
-        byte[] bytes = hexString.getBytes();
+        byte[] byteString = hexString.getBytes();
+        for(int i = 0; i < byteString.length; i++){
+            bytes[i] = byteString[i];
+        }
+        for(int i=19; i < 28; i++){
+            bytes[i] = 0x00;
+        }
+        bytes[29] = 0x00;
+        bytes[30] = 0x00;
+        bytes[31] = 0x00;
+        bytes[32] = 0x01;
+
         System.out.println(bytes.length);
         System.out.println(bytes.toString());
+        //bytes
         //File.WriteAllBytes("input.txt", StringToByteArray(hexString));
         System.out.println("Hello, World!");
     }
