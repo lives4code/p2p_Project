caa3b438a2aa4cb33012e8b4de8c378da0c11e07
lives4code <jacobmurleyis@gmail.com>
2021-09-20 16:01:28 -0400

    added methods to convert string to byte for handshake

--- Files_From_Prof/HelloWorld.java
+++ Files_From_Prof/HelloWorld.java
@@ -1,5 +1,12 @@
+import java.io.*;
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        String hexString = "P2PFILESHARINGPROJ";
+        byte[] bytes = hexString.getBytes();
+        System.out.println(bytes.length);
+        System.out.println(bytes.toString());
+        //File.WriteAllBytes("input.txt", StringToByteArray(hexString));
         System.out.println("Hello, World!");
     }
 }
