d39baa92c6aa874c72ae00fe4c2b8e9cf974dd6c
lives4code <jacobmurleyis@gmail.com>
2021-11-10 17:23:18 -0500

    updat bash script to automatically kill process after sleeping.

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+sleepTime=10s
 echo "---starting peers---"
 java PeerProcess 1001 &
+PID1=$!
 java PeerProcess 1002 &
+PID2=$!
 echo "---peers started---"
+sleep $sleepTime
+echo "killing processes after sleeping"
+echo $sleepTime
+kill $PID1
+kill $PID2
