0e55aab39ae5e54e63ac30a77928a54f1fe6de08
DrBubbles42 <22nickfox@gamil.com>
2021-12-04 02:05:11 -0500

    check script fix

--- src/checkResult.sh
+++ src/checkResult.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "---begin checking---"
+grep -Ev "have" out.txt | grep "PEER CHECK\|SERVER END\|download\|not done"; grep ERROR out.txt
 
 for id in {1002..1008}
 do
@@ -7,5 +9,4 @@ do
 	echo
 done
 
-grep -Ev "have" out.txt | grep "PEER CHECK\|SERVER END\|download\|not done"; grep ERROR out.txt
 echo "---done checking---"
