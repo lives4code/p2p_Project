7017c42c292b2b02814c8179e9a000fa1e712a29
DrBubbles42 <22nickfox@gamil.com>
2021-12-05 22:25:24 -0500

    added checkDiff.sh

---
+++ src/checkDiff.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "---begin checking---"
+
+for id in {1002..1003}
+do
+        echo "---diff $id---"
+        diff ../peers/1001/thefile ../peers/$id/thefile
+        echo
+done
+
+echo "---done checking---"
